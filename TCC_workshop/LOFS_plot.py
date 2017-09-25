6a72153a68e60f3e0247217a06ac8aee8e05aa38
jschroder <julien.schroder@gmail.com>
2017-09-25 15:38:54 -0800

    finale plots

--- TCC_workshop/LOFS_plot.py
+++ TCC_workshop/LOFS_plot.py
@@ -10,10 +10,10 @@ import glob
 
 rcParams[ 'xtick.direction' ] = 'out'
 rcParams[ 'ytick.direction' ] = 'out'
-rcParams[ 'xtick.labelsize' ] = 'X-large'
-rcParams[ 'ytick.labelsize' ] = 'X-large'
-rcParams[ 'figure.titlesize' ] = 'X-large'
-rcParams[ 'axes.titlesize' ] = 'large'
+rcParams[ 'xtick.labelsize' ] = 'XX-large'
+rcParams[ 'ytick.labelsize' ] = 'XX-large'
+rcParams[ 'figure.titlesize' ] = 'XX-large'
+rcParams[ 'axes.titlesize' ] = 'XX-large'
 rcParams[ 'axes.spines.top' ] = 'False'
 rcParams[ 'axes.spines.right' ] = 'False'
 rcParams[ 'savefig.dpi' ] = 1000
@@ -45,7 +45,7 @@ for scen in scenarios :
     df = pd.DataFrame(value,index = ['2010s','2020s','2030s','2040s','2050s','2060s','2070s','2080s','2090s'])
 
     df.columns = ["LOFS"]
-    ax = df.plot(kind='bar', legend=False)
+    ax = df.plot(kind='bar', legend=False, color='#8055A6')
 
     #stolen from http://robertmitchellv.com/blog-bar-chart-annotations-pandas-mpl.html                                                       
     totals = []
@@ -60,15 +60,16 @@ for scen in scenarios :
     # set individual bar lables using above list
     for i in ax.patches:
      # get_x pulls left or right; get_height pushes up or down
-     ax.text(i.get_x()+.05, i.get_height()-8, \
+     ax.text(i.get_x()+.03, i.get_height()+0.8, \
              str(round(i.get_height())),
-                 color='black',fontsize=16)
+                 color='black',fontsize=18)
     plt.xticks(rotation=0)
+    ax.yaxis.labelpad = 20
 
-    plt.ylabel('Length of Frozen Season (Days)',fontsize=17)
+    plt.ylabel('Length of Frozen Season (Days)',fontsize=25)
     # plt.title('Length of Frozen Season in the Yukon Flats Area,\n  CMIP5 - 5 Model Average - {}'.format(scen.upper()))
     # plt.show()
 
-    filename = os.path.join(pth_YK,"Plots", '_'.join(['lofs',scen,'plot','no_title']) + '.pdf')
+    filename = os.path.join(pth_YK,"Plots", '_'.join(['lofs',scen,'plot','no_title','edit']) + '.png')
     plt.savefig( filename )
     plt.close()
