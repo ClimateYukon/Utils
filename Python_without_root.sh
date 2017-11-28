8551ffd294547bb4f1150d38143077a2a103e80b
jschroder <julien.schroder@gmail.com>
2017-11-28 14:10:48 -0900

    Update Python_without_root.sh

--- Python_without_root.sh
+++ Python_without_root.sh
@@ -2,28 +2,28 @@
 mkdir ~/src
 mkdir ~/.localpython
 cd ~/src
-#wget https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tar.xz
-wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tar.xz
-#tar xvfJ Python-3.5.0.tar.xz
-tar xvfJ Python-2.7.10.tar.xz
-#cd Python-3.5.0
-cd Python-2.7.10
+wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
+
+tar xvfJ Python-3.6.3.tar.xz
+
+cd Python-3.6.3
+
 ​
 make clean
-./configure --prefix=/home/UA/jschroder/.localpython
+./configure --prefix=/home/UA/jschroder/.localpython --enable-shared
 make
 make install
 ​
 #2) Install virtualenv
 cd ~/src
-wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-13.1.2.tar.gz --no-check-certificate
-tar -zxvf virtualenv-13.1.2.tar.gz
-cd virtualenv-13.1.2/
+wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-15.1.0.tar.gz --no-check-certificate
+tar -zxvf virtualenv-15.1.0.tar.gz
+cd virtualenv-15.1.0/
 ~/.localpython/bin/python setup.py install
 ​
 #3) Create a virtualenv using your local python
 cd ~
-~/.localpython/bin/virtualenv venv2 --python=/home/UA/jschroder/.localpython/bin/python2.7
+~/.localpython/bin/virtualenv dod --python=/home/UA/jschroder/.localpython/bin/python3
 ​
 #4) Activate the environment
 ​
